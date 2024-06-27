789d4fdece62cc102f45b0ad5161e594ac791ef8
ahmadk-g <khalaf.ahmed@hotmail.com>
2024-06-27 16:16:31 +0200

    add queryset to views

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,5 @@ from .models import Post
 
 class PostList(generic.ListView):
     
-    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)  ## --> model = Post
     template_name = "post_list.html"
